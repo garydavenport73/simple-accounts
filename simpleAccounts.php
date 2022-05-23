411f6ffbe09733c4e358ef08659d2c6a7b202b28
garydavenport73 <software@garydavenport.com>
2022-05-22 21:09:12 -0400

    fixed php so returns nothing unless post

--- simpleAccounts.php
+++ simpleAccounts.php
@@ -3,15 +3,6 @@ header('Access-Control-Allow-Origin: *');
 // header('Access-Control-Allow-Methods: GET, POST');
 // header("Access-Control-Allow-Headers: X-Requested-With");
 ?>
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Account Management Message</title>
-</head>
 
 <?php
 
@@ -40,7 +31,6 @@ function emailPasswordMatch($email, $password)
     }
     return false;
 }
-
 
 function unexpiredToken($email, $minutes)
 {
@@ -77,48 +67,61 @@ function sendToken($email)
 }
 
 ?>
-
-<body>
-minimal response for any POST here
-    <?php
-    //var_dump($_POST);
-
-    if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-        if ($_POST["do-this"] === "login") {
-            echo "<h2>Login Test:</h2>";
-            $email = strtolower($_POST["email"]);
-            if (emailPasswordMatch($email, $_POST["password"])) {
-                echo "<p>The email and password match.</p>";
-            } else {
-                echo "<p>The email and password do not match.</p>";
-            };
-        } else if ($_POST["do-this"] === "update-profile") {
-            echo "<h2>Request Result:</h2>";
-            $email = strtolower($_POST["email"]);
-            $token = $_POST["token"];
-            if (emailTokenMatch($email, $token) && unexpiredToken($email, 10)) {
-                file_put_contents("users/$email/password", password_hash($_POST["password"], PASSWORD_DEFAULT));
-                unlink("users/$email/token");
-                echo "<p>Request was successful.</p>";
-            } else {
-                echo "<p>Request was not succuessful.</p>";
-            }
-        } else if ($_POST["do-this"] === "send-token") { //send a token
-            echo "<h2>Token Requested.</h2>";
-            $email = strtolower($_POST["email"]);
-            if (unexpiredToken("$email", 10)) {
-                //do nothing
-            } else {
-                $token = sendToken($email);
-            }
-            echo "<p>A request to send a token to $email has been made.</p>" .
-                "<p>Check your email $email for the token.</p>" . "(testing purposes: $token)</p>" .
-                "<p>Tokens are good for 10 minutes.</p>" .
-                "<p>(Note: if a token has already been issued in the last 10 minutes, another will not be sent.)</p>";
+<?php
+$webPagePart1 = "<!DOCTYPE html>
+    <html lang='en'>
+    <head>
+        <meta charset='UTF-8'>
+        <meta http-equiv='X-UA-Compatible' content='IE=edge'>
+        <meta name='viewport' content='width=device-width, initial-scale=1.0'>
+        <title>Account Management Message</title>
+    </head>
+    <body>";
+$webPagePart2 = "</body>
+    </html>";
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    echo "minimal response for any POST here<br>";
+    var_dump($_POST);
+    if ($_POST["do-this"] === "login") {
+        echo $webPagePart1;
+        echo "<h2>Login Test:</h2>";
+        $email = strtolower($_POST["email"]);
+        if (emailPasswordMatch($email, $_POST["password"])) {
+            echo "<p>The email and password match.</p>";
+        } else {
+            echo "<p>The email and password do not match.</p>";
+        };
+        echo $webPagePart2;
+        echo "<button onclick='window.history.back()'>OK</button>";
+    } else if ($_POST["do-this"] === "update-profile") {
+        echo $webPagePart1;
+        echo "<h2>Request Result:</h2>";
+        $email = strtolower($_POST["email"]);
+        $token = $_POST["token"];
+        if (emailTokenMatch($email, $token) && unexpiredToken($email, 10)) {
+            file_put_contents("users/$email/password", password_hash($_POST["password"], PASSWORD_DEFAULT));
+            unlink("users/$email/token");
+            echo "<p>Request was successful.</p>";
+        } else {
+            echo "<p>Request was not succuessful.</p>";
         }
+        echo $webPagePart2;
+        echo "<button onclick='window.history.back()'>OK</button>";
+    } else if ($_POST["do-this"] === "send-token") { //send a token
+        echo $webPagePart1;
+        echo "<h2>Token Requested.</h2>";
+        $email = strtolower($_POST["email"]);
+        if (unexpiredToken("$email", 10)) {
+            //do nothing
+        } else {
+            $token = sendToken($email);
+        }
+        echo "<p>A request to send a token to $email has been made.</p>" .
+            "<p>Check your email $email for the token.</p>" . "(testing purposes: $token)</p>" .
+            "<p>Tokens are good for 10 minutes.</p>" .
+            "<p>(Note: if a token has already been issued in the last 10 minutes, another will not be sent.)</p>";
+        echo $webPagePart2;
         echo "<button onclick='window.history.back()'>OK</button>";
     }
-    ?>
-</body>
-
-</html>
+}
+?>
